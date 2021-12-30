00a45a62fd7bc187a21c25d4c00037e1c2b0805a
Pranjal Chauhan <88274017+GauranshChauhan@users.noreply.github.com>
2021-12-30 15:25:32 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import { MoralisProvider } from "react-moralis"
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
-      appId='a0sg3GMs48IZoyqckZYI87JTaa8EDecTLz0GFxTx'
-      serverUrl='https://c0pj0sbut07i.usemoralis.com:2053/server'
+    appId='put your own appId by going to moralis dashboard'
+      serverUrl='put your own serverUrl by going to moralis dashboard'
     >
       <Component {...pageProps} />
     </MoralisProvider> 
